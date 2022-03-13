2e0f4de047a19449d1a3bcbe13bea93a97bee5f2
ashok <ashok215it@gmail.com>
2022-03-13 23:10:47 +0530

    new class file added

---
+++ src/org/text/CourseName.java
@@ -0,0 +1,15 @@
+package org.text;
+
+public class CourseName {
+	public void javaCourse() {
+System.out.println("java course is started");
+	}
+	public void pythonCourse() {
+System.out.println("python course is not started");
+	}
+	public static void main(String[] args) {
+		CourseName c = new CourseName();
+		c.javaCourse();
+		c.pythonCourse();
+	}
+}
